6f7677b20c6f78f745ea19af640191b59faeef15
Jan Kaul <jankaul@mailbox.org>
2024-11-28 21:03:12 +0100

    add object to schema

--- destination-iceberg/src/schema.rs
+++ destination-iceberg/src/schema.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use airbyte_protocol::schema::{Compound, Format, JsonSchema, Primitive, Type};
 use arrow::datatypes::{DataType, Field, Schema, SchemaBuilder, TimeUnit};
 
@@ -7,38 +9,61 @@ pub fn schema_to_arrow(schema: &JsonSchema) -> Result<Schema, Error> {
     if let Type::Compound(Compound::Object(object)) = &schema.r#type {
         let mut builder = SchemaBuilder::new();
         for (name, r#type) in &object.properties {
-            let field = match r#type {
-                Type::Primitive { r#type } => {
-                    Field::new(trim_name(name), primitive_to_arrow(r#type), false)
-                }
-                Type::Single { r#type } => {
-                    Field::new(trim_name(name), primitive_to_arrow(&r#type[0]), false)
-                }
-                Type::Variant { r#type } => {
-                    let value = match (&r#type[0], &r#type[1]) {
-                        (prim, Primitive::Null) => prim,
-                        (Primitive::Null, prim) => prim,
-                        (first, _) => first,
-                    };
-                    Field::new(trim_name(name), primitive_to_arrow(value), true)
-                }
-                Type::PrimitiveFormat { r#type: _, format } => {
-                    Field::new(trim_name(name), primitivedate_to_arrow(format), false)
-                }
-                Type::SingleFormat { r#type: _, format } => {
-                    Field::new(trim_name(name), primitivedate_to_arrow(format), false)
-                }
-                Type::VariantFormat { r#type: _, format } => {
-                    Field::new(trim_name(name), primitivedate_to_arrow(format), true)
-                }
-                Type::Compound(_) => Field::new(trim_name(name), DataType::Null, true),
-                Type::Empty(_) => Field::new(trim_name(name), DataType::Null, true),
-            };
+            let field = field_to_arrow(r#type, name);
             builder.push(field);
         }
         Ok(builder.finish())
     } else {
         Err(Error::NoSchema)
+    }
+}
+
+fn field_to_arrow(datatype: &Type, name: &String) -> Field {
+    match datatype {
+        Type::Primitive { r#type } => {
+            Field::new(trim_name(name), primitive_to_arrow(r#type), false)
+        }
+        Type::Single { r#type } => {
+            Field::new(trim_name(name), primitive_to_arrow(&r#type[0]), false)
+        }
+        Type::Variant { r#type } => {
+            let value = match (&r#type[0], &r#type[1]) {
+                (prim, Primitive::Null) => prim,
+                (Primitive::Null, prim) => prim,
+                (first, _) => first,
+            };
+            Field::new(trim_name(name), primitive_to_arrow(value), true)
+        }
+        Type::PrimitiveFormat { r#type: _, format } => {
+            Field::new(trim_name(name), primitivedate_to_arrow(format), false)
+        }
+        Type::SingleFormat { r#type: _, format } => {
+            Field::new(trim_name(name), primitivedate_to_arrow(format), false)
+        }
+        Type::VariantFormat { r#type: _, format } => {
+            Field::new(trim_name(name), primitivedate_to_arrow(format), true)
+        }
+        Type::Compound(Compound::Object(object)) => Field::new(
+            trim_name(name),
+            DataType::Struct(
+                object
+                    .properties
+                    .iter()
+                    .fold(SchemaBuilder::new(), |mut acc, (name, datatype)| {
+                        acc.push(field_to_arrow(&datatype, &name));
+                        acc
+                    })
+                    .finish()
+                    .fields,
+            ),
+            true,
+        ),
+        Type::Compound(Compound::Array(array)) => Field::new(
+            trim_name(name),
+            DataType::List(Arc::new(field_to_arrow(&*array.items, name))),
+            true,
+        ),
+        Type::Empty(_) => Field::new(trim_name(name), DataType::Null, true),
     }
 }
 
